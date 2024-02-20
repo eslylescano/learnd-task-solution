3fb4a5a72595edfe75f075cb56a905a79b237cab
Esly Lescano <eslylescano@gmail.com>
2024-02-20 16:48:52 +0000

    Add mock data that represent a database

--- main.go
+++ main.go
@@ -10,3 +10,15 @@ type PowerMeterData struct {
 	SerialID          string
 	ConsumptionPerDay float64
 }
+
+var powerMetters = map[string]Building{
+	"1111-1111-1111": {"Treatment Plant A", "Aquaflow", "1111-1111-1111"},
+	"1111-1111-2222": {"Treatment Plant B", "Aquaflow", "1111-1111-2222"},
+	"1111-1111-3333": {"Student Halls", "Albers Facilities Management", "1111-1111-3333"},
+}
+
+var meterConsumption = map[string]PowerMeterData{
+	"1111-1111-1111": {"1111-1111-1111", 20.0},
+	"1111-1111-2222": {"1111-1111-2222", 30.0},
+	"1111-1111-3333": {"1111-1111-3333", 40.0},
+}
